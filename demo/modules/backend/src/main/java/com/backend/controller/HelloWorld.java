23bd3dc862492690fef18a54891dd2b190a9640c
ahmed <ahmed.elghoul@infor.com>
2022-10-18 11:01:36 +0100

    first commit

---
+++ demo/modules/backend/src/main/java/com/backend/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.backend.controller;
+
+import com.backend.dto.HelloWorldDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class HelloWorld {
+
+  @GetMapping("/hello")
+  public HelloWorldDTO getHelloWorldMessage() {
+    HelloWorldDTO dto = new HelloWorldDTO();
+    dto.setMessage("Hello Spring with Angular");
+    return dto;
+  }
+}
