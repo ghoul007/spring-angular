948d4ce3258e2555a66baaafdb9d85bcd581ce76
ahmed <ahmed.elghoul@infor.com>
2022-10-19 23:53:05 +0100

    add liquibase

--- demo/modules/backend/src/main/java/com/backend/entity/Vehicule.java
+++ demo/modules/backend/src/main/java/com/backend/entity/Vehicule.java
@@ -1,13 +1,28 @@
 package com.backend.entity;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Vehicule")
 public class Vehicule  extends DistributedEntity{
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false)
   private String number;
+
+  @Column(nullable = true)
+  private String hello;
+
+
+  public String getHello() {
+    return hello;
+  }
+
+  public void setHello(String hello) {
+    this.hello = hello;
+  }
 
   public String getNumber() {
     return number;
