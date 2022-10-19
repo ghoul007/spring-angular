187f1c969c902d12951c31932f14501567597d6f
ahmed <ahmed.elghoul@infor.com>
2022-10-19 14:44:46 +0100

    add material design

--- demo/modules/frontend/src/app/vehicle/vehicle.component.ts
+++ demo/modules/frontend/src/app/vehicle/vehicle.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { PagedResponse } from '../models/paged-response-model';
 import { Vehicle } from '../models/vehicle.model';
 import { StompService } from '../services/stomp.service';
-// import { WebSocketService } from '../services/websocket.service';
 
 @Component({
   selector: 'app-vehicle',
@@ -11,27 +12,25 @@ import { StompService } from '../services/stomp.service';
   styleUrls: ['./vehicle.component.css']
 })
 export class VehicleComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'number', 'created', 'modified', 'action'];
+  page: number = 0;
+  size: number = 4;
+  totalCount:number = 0;
+
   vehicleForm = new FormGroup({
     number: new FormControl(''),
   });
 
   vehicles:Vehicle[] = [];
 
-  constructor(private http: HttpClient, private stompService: StompService ) {
-    http.get<Vehicle[]>("/api/vehicules/list").subscribe((response:Vehicle[]) => {
-      console.log("response", response);
-      this.vehicles = response;
-    });
-  }
+  constructor(private http: HttpClient, private stompService: StompService) {}
 
   ngOnInit(): void {
+    this.refreshVehicleTable(this.page, this.size);
+
     this.stompService.subscribe('/topic/vehicle', (): void => {
-      this.refreshVehicleTable();
+      this.refreshVehicleTable(this.page, this.size);
     });
-    // this.wsService.connect();
-    // this.wsService.messages$.subscribe((message: any) => {
-    //   console.log(message);
-    // });
   }
 
   onSubmit() {
@@ -46,9 +45,23 @@ export class VehicleComponent implements OnInit {
     })
   }
 
-  private refreshVehicleTable(): void {
-    this.http.get<Vehicle[]>("/api/vehicules/list").subscribe((response:Vehicle[]) => {
-      this.vehicles = response;
+  loadVehicles(event:PageEvent): PageEvent {
+    this.page = event.pageIndex;
+    this.size = event.pageSize;
+
+    this.refreshVehicleTable(this.page, this.size);
+    return event;
+  }
+
+  private refreshVehicleTable(page?:number, size?:number): void {
+    this.vehicles = [];
+    const sufix:string = (page !== undefined && size != undefined)
+        ? "?page=" + page + "&size=" + size
+        : "";
+    
+    this.http.get<PagedResponse<Vehicle>>("/api/vehicules/list" + sufix).subscribe((response:PagedResponse<Vehicle>) => {
+      this.vehicles = !!response.content ? response.content : [];
+      this.totalCount = !!response.totalCount ? response.totalCount : 0;
     });
   }
 }
